dffd5f803efd399db84d72a5083b54f40bfad9ff
rurigrass <ruripg@gmail.com>
2024-02-04 21:17:11 +0100

    added external js page and prettier

---
+++ script.js
@@ -0,0 +1,10 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(30, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
