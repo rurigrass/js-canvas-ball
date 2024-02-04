a39bca374df9b4553aa83751e99224fc653c629a
rurigrass <ruripg@gmail.com>
2024-02-04 21:39:45 +0100

    blla bounces off all the edges

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById("canvas");
+var canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-const ctx = canvas.getContext("2d");
+let counter = 0;
 
-ctx.beginPath();
-ctx.rect(30, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+    counter++;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+    counter++;
+  }
+
+  x += dx;
+  y += dy;
+  //   console.log(counter);
+}
+
+setInterval(draw, 10);
